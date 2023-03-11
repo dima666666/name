36de870e810473444de5d8635e4eedd7e533f62a
dima666666 <125134368+dima666666@users.noreply.github.com>
2023-03-11 17:22:53 +0300

    Add files via upload

---
+++ Untitled22 (1).py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+import matplotlib.pyplot as plt 
+x=["apple","samsung","poco","honor"]
+y=[16,5106,1206,1506]
+plt.bar(x,y)
+plt.xlabel("oсь X")
+plt.xlabel("oсь y")
+plt.title("мой график")
+plt.show
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
